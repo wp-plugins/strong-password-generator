5c6892f7e561cd0ade2400e80b5968603ddaea15
fjarrett <fjarrett@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 18:33:38 +0000

    Update to commit 4bf0d02 from git@github.com:fjarrett/strong-password-generator.git
    git-svn-id: https://plugins.svn.wordpress.org/strong-password-generator/tags/0.2.0@1153163 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Strong Password Generator ===
+Contributors:      fjarrett
+Tags:              admin, login, membership, passwords, profile, security, users
+Requires at least: 3.7
+Tested up to:      4.2
+Stable tag:        0.2.0
+License:           GPLv3
+License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+
+Encourage the use of strong passwords by helping users generate them easily.
+
+== Description ==
+
+**Did you find this plugin helpful? Please consider [leaving a 5-star review](https://wordpress.org/support/view/plugin-reviews/strong-password-generator).**
+
+Strong passwords are one of the best defenses for maintaining a secure website.
+
+The **Password Generator** button makes it easy for your users to choose a secure password.
+
+This plugin utilizes the [password-generator](https://github.com/bermi/password-generator) JavaScript library under the MIT license.
+
+**Development of this plugin is done [on GitHub](https://github.com/fjarrett/strong-password-generator). Pull requests welcome. Please see [issues reported](https://github.com/fjarrett/strong-password-generator/issues) there before going to the plugin forum.**
+
+== Screenshots ==
+
+1. When choosing a password, users can optionally choose to have a strong password generated for them.
+2. The fields are filled in automatically and the user is prompted to save their new password.
+3. The strong password generator is also available when editing profiles.
+
+== Changelog ==
+
+= 0.2.0 - May 4, 2015 =
+
+* Add slider for custom password length control, retire alert window
+
+Props [@fjarrett](https://github.com/fjarrett)
+
+= 0.1.0 - May 4, 2015 =
+
+* Initial release
+
+Props [@fjarrett](https://github.com/fjarrett)
---
+++ strong-password-generator.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Plugin Name: Strong Password Generator
+ * Description: Encourage the use of strong passwords by helping users generate them easily.
+ * Version: 0.2.0
+ * Author: Frankie Jarrett
+ * Author URI: http://frankiejarrett.com
+ * License: GPLv3
+ * Text Domain: strong-password-generator
+ */
+
+class Strong_Password_Generator {
+
+	/**
+	 * Plugin version number
+	 *
+	 * @const string
+	 */
+	const VERSION = '0.2.0';
+
+	/**
+	 * Hold plugin instance
+	 *
+	 * @var string
+	 */
+	public static $instance;
+
+	/**
+	 * Class constructor
+	 */
+	private function __construct() {
+		define( 'STRONG_PASSWORD_GENERATOR_PLUGIN', plugin_basename( __FILE__ ) );
+		define( 'STRONG_PASSWORD_GENERATOR_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'STRONG_PASSWORD_GENERATOR_URL', plugin_dir_url( __FILE__ ) );
+		define( 'STRONG_PASSWORD_GENERATOR_INC_DIR', STRONG_PASSWORD_GENERATOR_DIR . 'includes/' );
+		define( 'STRONG_PASSWORD_GENERATOR_LANG_PATH', dirname( STRONG_PASSWORD_GENERATOR_PLUGIN ) . '/languages' );
+
+		add_action( 'plugins_loaded', array( __CLASS__, 'i18n' ) );
+		add_action( 'init', array( __CLASS__, 'load' ) );
+		add_action( 'init', array( __CLASS__, 'register_scripts' ) );
+	}
+
+	/**
+	 * Load languages
+	 *
+	 * @action plugins_loaded
+	 *
+	 * @return void
+	 */
+	public static function i18n() {
+		load_plugin_textdomain( 'strong-password-generator', false, STRONG_PASSWORD_GENERATOR_LANG_PATH );
+	}
+
+	/**
+	 * Register hooks
+	 *
+	 * @action init
+	 *
+	 * @return void
+	 */
+	public static function load() {
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+		add_action( 'login_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * Register scripts and styles
+	 *
+	 * @action init
+	 *
+	 * @return void
+	 */
+	public static function register_scripts() {
+		wp_register_script( 'spg-password-generator', STRONG_PASSWORD_GENERATOR_URL . 'js/password-generator.min.js', array(), '0.2.1', true );
+		wp_register_script( 'spg-button', STRONG_PASSWORD_GENERATOR_URL . 'js/button.js', array( 'jquery', 'spg-password-generator' ), self::VERSION, true );
+		wp_register_style( 'spg-button', STRONG_PASSWORD_GENERATOR_URL . 'css/button.css', array(), self::VERSION );
+	}
+
+	/**
+	 * Enqueue scripts in the admin and login screens
+	 *
+	 * @action admin_enqueue_scripts
+	 * @action login_enqueue_scripts
+	 *
+	 * @return void
+	 */
+	public static function enqueue_scripts( $hook ) {
+		if (
+			'admin_enqueue_scripts' === current_action()
+			&&
+			! in_array( $hook, array( 'profile.php', 'user-edit.php', 'user-new.php' ) )
+		) {
+			return;
+		}
+
+		wp_enqueue_script( 'spg-password-generator' );
+		wp_enqueue_script( 'spg-button' );
+		wp_enqueue_style( 'spg-button' );
+
+		/**
+		 * Filter the default password length
+		 *
+		 * @return int
+		 */
+		$length = apply_filters( 'spg_default_password_length', 20 );
+
+		/**
+		 * Filter the minimum password length allowed
+		 *
+		 * @return int
+		 */
+		$min = apply_filters( 'spg_min_password_length', 7 );
+
+		/**
+		 * Filter the maximum password length allowed
+		 *
+		 * @return int
+		 */
+		$max = apply_filters( 'spg_max_password_length', 32 );
+
+		/**
+		 * Filter whether or not to allow memorable passwords (true = decreased entropy)
+		 *
+		 * @return bool
+		 */
+		$memorable = apply_filters( 'spg_allow_memorable_passwords', false );
+
+		wp_localize_script(
+			'spg-button',
+			'spg_button',
+			array(
+				'length'    => absint( $length ),
+				'min'       => absint( $min ),
+				'max'       => absint( $max ),
+				'memorable' => (bool) $memorable,
+				'i18n'      => array(
+					'button' => esc_html__( 'Password Generator', 'strong-password-generator' ),
+					'range'  => esc_html__( 'Length', 'strong-password-generator' ),
+				),
+			)
+		);
+	}
+
+	/**
+	 * Return active instance of Strong_Password_Generator, create one if it doesn't exist
+	 *
+	 * @return Strong_Password_Generator
+	 */
+	public static function get_instance() {
+		if ( empty( self::$instance ) ) {
+			$class = __CLASS__;
+			self::$instance = new $class;
+		}
+
+		return self::$instance;
+	}
+
+}
+
+$GLOBALS['strong_password_generator'] = Strong_Password_Generator::get_instance();
